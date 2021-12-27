ca710254ebcb5c8883f532e577e9dabb3c62572c
Tomoyuki Sakurai <y@trombik.org>
2021-12-27 11:14:34 +0700

    initial implementation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ansible-role-vif
+# `trombik.vif`
 
-A brief description of the role goes here.
+An `ansible` role to create virtual interfaces on OpenBSD.
 
 # Requirements
 
@@ -10,7 +10,18 @@ None
 
 | variable | description | default |
 |----------|-------------|---------|
+| `vif_config` | A list of virtual network interfaces (see  below) | `[]` |
 
+## `vif_config`
+
+This variable is a list of virtual interfaces to create. Its element is a
+dict.
+
+| Key | Description | Mandatory? |
+|-----|-------------|------------|
+| `name` | The name of interface. | Yes |
+| `priority` | Priority in integer. Interfaces with a higher priority are created earlier. | Yes |
+| `config` | The content of `hostname.if(5)` | Yes |
 
 # Dependencies
 
@@ -19,6 +30,30 @@ None
 # Example Playbook
 
 ```yaml
+---
+- hosts: localhost
+  roles:
+    - ansible-role-vif
+  vars:
+    vif_config:
+      - name: trunk0
+        priority: 20
+        config: |
+          trunkport vether0
+          trunkport vether1
+          10.1.1.100/24
+      - name: vether0
+        priority: 100
+        config: |
+          up
+      - name: vether1
+        priority: 100
+        config: |
+          up
+      - name: carp0
+        priority: 10
+        config: |
+          10.0.2.100/24 carpdev em0 vhid 1 pass password
 ```
 
 # License
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,30 @@
 require "spec_helper"
 require "serverspec"
 
-package = "vif"
-service = "vif"
-config  = "/etc/vif/vif.conf"
-user    = "vif"
-group   = "vif"
-ports   = [PORTS]
-log_dir = "/var/log/vif"
-db_dir  = "/var/lib/vif"
+vifs = [
+  { name: "carp0", inet: "10.0.2.100" },
+  { name: "trunk0", inet: "10.1.1.100", extra_regexp: [/vether\d port master,active/, /vether\d port active/] },
+  { name: "vether0", inet: false },
+  { name: "vether1", inet: false }
+]
 
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/vif.conf"
-  db_dir = "/var/db/vif"
-end
-
-describe package(package) do
-  it { should be_installed }
-end
-
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("vif") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/vif") do
+vifs.each do |vif|
+  describe file "/etc/hostname.#{vif[:name]}" do
+    it { should exist }
     it { should be_file }
+    it { should be_mode 600 }
+    it { should be_owned_by "root" }
+    it { should be_grouped_into "wheel" }
   end
-end
 
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
+  describe command "ifconfig #{vif[:name]}" do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/^#{vif[:name]}:\s+.*UP.*RUNNING/) }
+    its(:stdout) { should match(/inet #{vif[:inet]}/) } if vif[:inet]
+    if vif.key?(:extra_regexp)
+      vif[:extra_regexp].each do |re|
+        its(:stdout) { should match(/#{re}/) }
+      end
+    end
   end
 end
